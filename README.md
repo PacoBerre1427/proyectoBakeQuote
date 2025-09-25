5bc330248aa5cb649e7242224135935b6a7cba64
GaritoChavarria <edgar.chavarria0928@gmail.com>
2025-09-25 00:34:34 -0700

    Configuracion de NavigationView y modificaciones a SecondActivity

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Proyecto móvil: Bake & Quote
 ##SAÚL ALBERTO BERRELLEZA PACO, EDGAR JOSUE CHAVARRIA NAVARRO
-##Actualización 17/09/2025
+##Fecha 17/09/2025
 * Se agregó una segunda clase "SecondActivity" la cual es una clase que abre otra ventana "activity_second", esto se ejecutará una vez presionado el botón de "activity_main", el cual, su evento se encuentre en la clase "MainActivity". El diseño del "activity_main.xml", consta de un color degradado de fondo y dos campos de texto para capturar el correo y contraseña (con sus formatos en concreto), una casilla para recordar la contraseña y el botón antes mencionado.
 ##Actualizaciones 24/09/2025
 * Se agregaron iconos para los campos de texto y el logo de la aplicación.
--- app/src/main/java/com/example/proyectobakeandquote/SecondActivity.java
+++ app/src/main/java/com/example/proyectobakeandquote/SecondActivity.java
@@ -1,24 +1,84 @@
 package com.example.proyectobakeandquote;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+import androidx.activity.EdgeToEdge;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Activar edge to edge
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_second);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        // Configurar el toggle para abrir/cerrar el Drawer usando el ícono hamburguesa
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Listener para el menú lateral
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.nav_home) {
+                    // Acción para inicio
+                } else if (id == R.id.nav_settings) {
+                    // Acción para configuración
+                } else if (id == R.id.nav_logout) {
+                    // Acción para salir
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        // Ajustar padding para edge-to-edge a la vista principal
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
